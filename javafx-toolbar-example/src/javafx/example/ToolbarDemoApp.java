a66a9b61dd778fc784050f9deb8e962fbfa24cf6
Rajeev Kumar Singh <rajeevs@flock.co>
2017-05-05 19:30:52 +0530

    javafx toolbar example

---
+++ javafx-toolbar-example/src/javafx/example/ToolbarDemoApp.java
@@ -0,0 +1,23 @@
+package javafx.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ToolbarDemoApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("toolbar-demo.fxml"));
+        primaryStage.setTitle("Toolbar Demo App");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
