d0a1a9cf7ac634508a47db5bfb70124947c36415
Rajeev Kumar Singh <rajeevs@flock.co>
2017-08-26 15:20:46 +0530

    removed unused imports

--- javafx-built-in-layout-pane-examples/src/javafx/example/borderpane/BorderPaneApplication.java
+++ javafx-built-in-layout-pane-examples/src/javafx/example/borderpane/BorderPaneApplication.java
@@ -4,12 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
